fa0dc2f6fbecda619b09c4bffdbed3925e296824
noncon66 <klaus.nu@cable.vol.at>
2021-04-22 21:04:11 +0200

    tried router

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express'); //import express app
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 //const cors = require('cors');
 const path = require('path');
+
 const app = express(); //instanciate express app
-app.use(bodyParser.urlencoded({ extended: true }));
+const router = express.router();
+//app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3030; //listening to port 3000
 const fs = require('fs');
 
-const name = "karl";
-searchFile(name);
+//const name = "karl";
+//searchFile(name);
+
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/test.html'));
+});
 
 
 app.post('/login', function (req, res){
     console.log('Got body:', req.body);
-    res.sendStatus(200);
+    const name = req.body;
+    searchFile(name);
+    res.send(name);
 
 });
+
 
 
 
@@ -50,6 +60,8 @@ function searchFile(name) {
 function createSession(name) {
     console.log("Session created")
 }
+
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
