61700dec7a84db18dac0b5f927eaa6a98cfc7b88
hall <danny.hall@illwerkevkw.at>
2021-04-22 21:02:07 +0200

    Add nodmon

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express'); //import express app
-const bodyParser = require('body-parser');
+const app = express(); //instanciate express app
+//const bodyParser = require('body-parser');
 //const cors = require('cors');
 const path = require('path');
-const app = express(); //instanciate express app
-app.use(bodyParser.urlencoded({ extended: true }));
+const router = express.Router();
+
+//app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3030; //listening to port 3000
 const fs = require('fs');
 
@@ -12,7 +14,7 @@ searchFile(name);
 
 
 app.post('/login', function (req, res){
-    console.log('Got body:', req.body);
+    console.log('Got body:', req);
     res.sendStatus(200);
 
 });
@@ -50,6 +52,23 @@ function searchFile(name) {
 function createSession(name) {
     console.log("Session created")
 }
+
+
+
+
+// routes
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+
+});
+  
+router.get('/home',function(req,res){
+    res.sendFile(path.join(__dirname+'/home.html'));
+  });
+
+app.use('/', router);
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
