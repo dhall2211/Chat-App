c6c1b0726a5409443a73045c0e49424959944292
noncon66 <klaus.nu@cable.vol.at>
2021-04-27 21:54:18 +0200

    added get and send Chat

--- server.js
+++ server.js
@@ -1,42 +1,71 @@
 const express = require('express'); //import express app
-var session = require('express-session')
+var session = require('express-session') //for session handling
 const app = express(); //instanciate express app
-const path = require('path');
-const router = express.Router();
+const path = require('path'); // path handling
+const router = express.Router(); //for route handling
 
 
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json()); //json capabilities
+
+app.use(express.urlencoded({
+    extended: true
+})); // parses incoming requests with urlencoded payload
+
 const port = 3030; //listening to port 3000
-const fs = require('fs');
-const port = 3030; //listening to port 3000
+const fs = require('fs'); //for file handling
 
 app.use(express.static('public'));
 app.use('/', router);
-app.use(express.urlencoded({ extended: true }));
+
 app.use(session({
     secret: 'einhorn chat',
     resave: false,
     saveUninitialized: true
-  }))
+}))
 
-  
- 
-app.post('/login', function (req, res, next){
+const chats = [];
+
+app.post('/login', function (req, res, next) {
     console.log('Got body:', req.body);
     req.session.username = req.body.name;
-    
+
     res
-    .status(201)
-    .redirect(301, '/home')
+        .status(201)
+        .redirect(301, '/home')
 
 });
 
 // post logout
 
 // get chats
-
+app.get('/getChat', function (req, res) {
+    res.json(chats);
+    }
+);
 
 // post chat
+app.post('/sendChat', function (req, res, next) {
+    //console.log('Cookies: ', req.cookies);
+    console.log('The Body:', req.body);
+    console.log('Session: ', req.session);
+
+    const name = req.cookies.user;
+    const msg = req.body.msg;
+    const timestamp = Date.now();
+    chats.push({
+        'user': name,
+        'msg': msg,
+        'timestamp': timestamp
+    })
+    console.log(chats);
+    res
+        .status(200)
+        .json(chats);
+
+});
+
+
 
 
 // file logic
@@ -69,16 +98,16 @@ function searchAndCreateFile(name) {
 
 // routes
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/pages/index.html'));
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
 
 });
-  
-router.get('/home',function(req,res){
-    res.sendFile(path.join(__dirname+'/pages/home.html'));
-  });
+
+router.get('/home', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/home.html'));
+});
 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  });
+});
