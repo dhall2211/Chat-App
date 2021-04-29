2bad779bbf87975021b469645c1e6d1b8ba90e92
dhall2211 <dhall2211@gmail.com>
2021-04-29 21:24:32 +0200

    Add connection

--- repository/databaseConnection.js
+++ repository/databaseConnection.js
@@ -1,18 +1,16 @@
 const mysql = require("mysql");
+require('dotenv').config();
+
+
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "me",
-  password: "secret",
-  database: "my_db",
+  port: "3306",
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: "chatapp",
 });
-
+console.log(process.env.USER, process.env.PASSWORD);
 
 // test
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+
 module.exports = connection;
